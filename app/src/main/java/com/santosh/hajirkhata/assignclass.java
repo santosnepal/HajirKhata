8587713d5102e71a3286daf90e4b3588e86bfe43
santosh <santoshnepal608@gmail.com>
2019-11-18 20:18:42 +0545

    class assiggn successul

--- app/src/main/java/com/santosh/hajirkhata/assignclass.java
+++ app/src/main/java/com/santosh/hajirkhata/assignclass.java
@@ -1,17 +1,22 @@
 package com.santosh.hajirkhata;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
@@ -22,17 +27,23 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class assignclass extends AppCompatActivity {
     Spinner faculty,program,semester,subjects;
     ProgressBar load;
     Button add;
+    String selfaculty,selprogram,selsemester,selsubjects;
     FirebaseFirestore db=FirebaseFirestore.getInstance();
-    CollectionReference dref;
+    CollectionReference dref,cref;
+    //DocumentReference pref,ppref;
+    int iddd;
     ArrayList<String> sfaculty=new ArrayList<String>();//{"Choose Faculty"};
+    ArrayList<String> fid=new ArrayList<String>();
     ArrayList<String> sprogram=new ArrayList<String>();//{"Choose Program"};
     ArrayList<String> ssemester=new ArrayList<String>();//{"Choose semester"};
-    ArrayList<String> ssubjects=new ArrayList<>();//{"Choose Subjects"};
+    ArrayList<String> ssubjects=new ArrayList<String>();//{"Choose Subjects"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,6 +58,7 @@ public class assignclass extends AppCompatActivity {
         sfaculty.add("Choose Faculty");
         sprogram.add("Choose Program");
         ssemester.add("Choose Semester");
+        ssubjects.add("Choose Subjects");
         sprogram.add("BCA");
         sprogram.add("BIT");
         ssemester.add("I");
@@ -74,7 +86,76 @@ public class assignclass extends AppCompatActivity {
                 android.R.layout.simple_spinner_item,ssubjects);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         subjects.setAdapter(sadapter);
+        faculty.setVisibility(View.GONE);
+        program.setVisibility(View.GONE);
+        semester.setVisibility(View.GONE);
+        subjects.setVisibility(View.GONE);
+        add.setVisibility(View.GONE);
         loadfaculty();
+       faculty.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+           @Override
+           public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+               selfaculty=faculty.getSelectedItem().toString();
+               if (!selfaculty.equals("Choose Faculty")){
+                   program.setVisibility(View.VISIBLE);
+               }
+               else {
+                   program.setVisibility(View.GONE);
+                   semester.setVisibility(View.GONE);
+                   add.setVisibility(View.GONE);
+               }
+           }
+
+           @Override
+           public void onNothingSelected(AdapterView<?> adapterView) {
+
+           }
+       });
+       program.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+           @Override
+           public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+               selprogram=program.getSelectedItem().toString();
+               if(!selprogram.equals("Choose Program")){
+                   semester.setVisibility(View.VISIBLE);
+               }
+               else {
+                   semester.setVisibility(View.GONE);
+                   subjects.setVisibility(View.GONE);
+                   add.setVisibility(View.GONE);
+               }
+           }
+
+           @Override
+           public void onNothingSelected(AdapterView<?> adapterView) {
+
+           }
+       });
+       semester.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+           @Override
+           public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+               selsemester=semester.getSelectedItem().toString();
+               if(!selsemester.equals("Choose Semester")){
+                   subjects.setVisibility(View.VISIBLE);
+                   showsub();
+               }
+               else {
+                   subjects.setVisibility(View.GONE);
+                   add.setVisibility(View.GONE);
+               }
+           }
+
+           @Override
+           public void onNothingSelected(AdapterView<?> adapterView) {
+
+           }
+       });
+       add.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               assign();
+           }
+       });
+
     }
     public void hide(){
         load.setVisibility(View.VISIBLE);
@@ -95,7 +176,8 @@ public class assignclass extends AppCompatActivity {
 
     }
     public void loadfaculty(){
-        hide();
+        //hide();
+        load.setVisibility(View.VISIBLE);
         dref=db.collection("faculty");
         dref.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
             @Override
@@ -107,14 +189,89 @@ public class assignclass extends AppCompatActivity {
                     String us=fs.getUsername();
                     String f= ns+" "+ls+"( "+us+" )";
                     sfaculty.add(f);
+                    fid.add(us);
 
 
 
                 }
+                faculty.setVisibility(View.VISIBLE);
             }
         });
-        show();
+        //show();
+        load.setVisibility(View.GONE);
 
+    }
+    public void showsub(){
+        String from=selprogram+"-"+selsemester;
+        cref=db.collection(from);
+        cref.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                for(QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots){
+                    subjects sss=documentSnapshot.toObject(subjects.class);
+                    Boolean bbb=sss.getAssigned();
+                    if (bbb){}
+                    else {
+                        String sub1=sss.getCode();
+                        ssubjects.add(sub1);
+                    }
+                }
+                add.setVisibility(View.VISIBLE);
+
+            }
+        });
+
+    }
+    public void assign(){
+        iddd=faculty.getSelectedItemPosition()-1;
+        String ddd=fid.get(iddd);
+        load.setVisibility(View.VISIBLE);
+        faculty.setVisibility(View.GONE);
+        program.setVisibility(View.GONE);
+        semester.setVisibility(View.GONE);
+        subjects.setVisibility(View.GONE);
+        add.setVisibility(View.GONE);
+        Map<String,String>  ass=new HashMap<>();
+        ass.put("code",subjects.getSelectedItem().toString());
+        ass.put("program",program.getSelectedItem().toString());
+        ass.put("semester",semester.getSelectedItem().toString());
+        ass.put("faculty",ddd);
+        final Map<String,Boolean> fix=new HashMap<>();
+        //fix.put("assigned",true);
+        db.collection("assigned").document(program.getSelectedItem().toString()+"-"+subjects.getSelectedItem().toString()).set(ass).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                db.collection(program.getSelectedItem().toString()+"-"+semester.getSelectedItem().toString()).document(subjects.getSelectedItem().toString()).update("assigned",true).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        showalert("class assigned successfully");
+                        finish();
+
+                    }
+                });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                showalert("Sorry Error occoured during assignign class Please try again later");
+                finish();
+            }
+        });
+
+
+
+    }
+    public void showalert(String message){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage(message);
+        alertDialogBuilder.setNeutralButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        AlertDialog ald=alertDialogBuilder.create();
+        ald.show();
     }
 
 }
---
+++ app/src/main/java/com/santosh/hajirkhata/subjects.java
@@ -0,0 +1,20 @@
+package com.santosh.hajirkhata;
+
+public class subjects {
+    public Boolean assigned;
+    public String ffaculty;
+    public String code;
+    public subjects(){}
+
+    public Boolean getAssigned() {
+        return assigned;
+    }
+
+    public String getFfaculty() {
+        return ffaculty;
+    }
+
+    public String getCode() {
+        return code;
+    }
+}
